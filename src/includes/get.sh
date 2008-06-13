e5ddd17c14f1a2036e5f822cb5102a3ea8a7bbae
alister.lewisbowen <alister.lewisbowen@0629b1d6-8a4e-0410-b2ad-29afed616e54>
2008-06-13 02:46:25 +0000

    build in timestamp and todo list

--- src/includes/get.sh
+++ src/includes/get.sh
@@ -1,4 +1,4 @@
-# Function: Retrieve/get a context within the Drupal source tree
+# Function: Pull a context into the Drupal source tree
 # Usage: get [core|module|theme] [project] [branch] [version]
 # ----------------------------------------------------------------------------
 
@@ -62,76 +62,6 @@ function get() {
 						
 		profile )
 			profile sample;;
-			
-	esac;
-}
-
-# Function: Update a context within the Drupal source tree
-# Usage: update [core|all] [branch]
-# Usage: update [module|theme|all] [project|all] [branch]
-# ----------------------------------------------------------------------------
-
-function update() {
-	local _context=${1:-$CONTEXT};
-	local _project=${2:-$PROJECT};
-	local _branch=${3:-$BRANCH};
-	
-	case "$_context" in
-	
-		'' | all )
-			update core;
-			update module;
-			#update theme;
-			;;
-			
-		core )
-			cd $TREE_CORE;
-			case "$_branch" in
-				'' | all )
-					for _dir in `ls`; do
-						cvsUpCore $_dir; 
-					done;;
-				* )
-					cvsUpCore $_branch;;	
-			esac;;
-			
-		module | theme )
-			if [ "$_context" == 'module' ]; then 
-				_dir=$TREE_MODULE;
-				_cvsCo=cvsCoModule;
-			else 
-				_dir=$TREE_THEME;
-				_cvsCo=cvsCoTheme; 
-			fi;
-
-			cd $_dir;
-			case "$_project" in
-				'' | all )
-					if [[ -d $_branch && "$_branch" ]]; then
-						cd $_branch;
-						for _project in `ls`; do
-							$_cvsCo $_project $_branch;
-						done;
-					else
-						for _i in `ls`; do
-							cd $_dir/$_i;
-							for _project in `ls`; do
-								$_cvsCo $_project $_i;
-							done;
-						done;
-					fi;;	
-				* )
-					if [ "$_branch" ]; then
-						$_cvsCo $_project $_branch;
-					else
-						for _i in `find . | egrep $_project\\.info | cut -d'/' -f2`; do
-							$_cvsCo $_project $_i; 
-						done;
-					fi;; 
-			esac;;
-			
-		profile )
-			profile update;;
 			
 	esac;
 }
